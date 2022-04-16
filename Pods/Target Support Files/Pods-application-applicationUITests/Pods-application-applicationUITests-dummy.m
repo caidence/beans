1981f492b2b267decaacbb0c0a84b67afe9a1b47
caidence <81723187+caidence@users.noreply.github.com>
2022-04-15 21:21:34 -0500

    Delete Pods directory

--- Pods/Target Support Files/Pods-application-applicationUITests/Pods-application-applicationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_application_applicationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_application_applicationUITests
-@end
--- Pods/Target Support Files/Pods-applicationTests/Pods-applicationTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_applicationTests : NSObject
-@end
-@implementation PodsDummy_Pods_applicationTests
-@end
